acd0f0b9c45f89cbfb34f8e63895aaf38475812e
Faith <faithfulgodwinc@gmail.com>
2024-09-08 02:03:53 -0700

    upgrade to web app

--- functionality.js
+++ functionality.js
@@ -286,12 +286,14 @@ function Reset() {
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
 
   // Add content to the cells
   cell1.textContent = '1';
   cell2.innerHTML = '<input type="text" class="table-input">';
   cell3.innerHTML = '<input type="number" min="1" class="table-input">';
   cell4.innerHTML = '<input type="text" maxlength="1" class="table-input">';
+  cell5.innerHTML = '<button class="delete-button">Remove</button>'
 
   // Clear the display result
   document.getElementById('display-result').textContent = '';
